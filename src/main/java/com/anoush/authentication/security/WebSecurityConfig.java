ca89b8e9b17dc69cd5298c3c5a8f3e1d36157a18
Manoj Chaulagain <machaulagain@sbgtv.com>
2020-10-23 18:07:06 -0700

    Swagger Changes

--- src/main/java/com/anoush/authentication/security/WebSecurityConfig.java
+++ src/main/java/com/anoush/authentication/security/WebSecurityConfig.java
@@ -58,9 +58,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
-                .antMatchers("/api/auth/**").permitAll()
-                .antMatchers( "/graphiql/**" ).permitAll()
-                .antMatchers( "/graphiql" ).permitAll()
+            .antMatchers("/api/auth/**").permitAll()
+            .antMatchers("/graphiql/**").permitAll()
+            .antMatchers("/graphiql").permitAll()
+            .antMatchers("/api-docs/**").permitAll()
+            .antMatchers("/swagger-ui/**").permitAll()
+            .antMatchers("/swagger-ui-custom.html/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
